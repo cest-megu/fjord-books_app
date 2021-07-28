8da5cc8b12842b2a50b43a09ff0ee79a331c0fe3
Megurin <tutimur_0521@yahoo.co.jp>
2021-07-28 19:43:41 +0900

    localesの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,16 @@ Bundler.require(*Rails.groups)
 
 module BooksApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    around_action :switch_locale
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.load_defaults 6.1
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+
+    def switch_locale(&action)
+      locale = params[:locale] || I18n.default_locale
+      I18n.with_locale(locale, &action)
+    end
+
   end
 end
