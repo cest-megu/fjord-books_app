fab5b6d4d083d1506e6da27375d5b6ed2a151c60
Megurin <tutimur_0521@yahoo.co.jp>
2021-07-28 20:50:41 +0900

    viewファイルの国際化

--- config/application.rb
+++ config/application.rb
@@ -8,16 +8,10 @@ Bundler.require(*Rails.groups)
 
 module BooksApp
   class Application < Rails::Application
-    around_action :switch_locale
 
     config.load_defaults 6.1
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
-
-    def switch_locale(&action)
-      locale = params[:locale] || I18n.default_locale
-      I18n.with_locale(locale, &action)
-    end
 
   end
 end
