867ae8431f44afebdc6f6fe5395fa8f68192aa92
Megurin <tutimur_0521@yahoo.co.jp>
2021-08-09 09:32:08 +0900

    carrierwaveの設定削除

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -31,17 +31,15 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process resize_to_fit: [100, 100]
-  end
-
-  process resize_to_limit: [200, 300]
+  # version :thumb do
+  #   process resize_to_fit: [100, 100]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_whitelist
-    %w[jpg jpeg gif png]
-  end
+  # def extension_whitelist
+  #   %w[jpg jpeg gif png]
+  # end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
@@ -49,7 +47,4 @@ class PictureUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
 
-  def size_range
-    0..5.megabytes
-  end
 end
