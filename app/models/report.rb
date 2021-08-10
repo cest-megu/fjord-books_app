58adc8801fe2a80ee9b70b9f973cf346551c998f
Megurin <tutimur_0521@yahoo.co.jp>
2021-08-10 10:53:15 +0900

    reports_controllerのcurrent_user紐付け

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,6 +2,9 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
+  validates :title, presence: true
+  validates :content, presence: true
+
   def ensure_user?(logined_user)
     user == logined_user
   end
