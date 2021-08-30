b0c87ba6e2d144f31291df12b1ecaed88f56dd3b
Megurin <tutimur_0521@yahoo.co.jp>
2021-08-30 15:36:08 +0900

    report test追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,13 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#editable?(target_user)" do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    report = Report.new(title: "今日の課題", content: "テスト書く", user_id: me.id)
+    assert report.editable?(me)
+  end
+
+  test "#created_on" do
+
+  end
 end
