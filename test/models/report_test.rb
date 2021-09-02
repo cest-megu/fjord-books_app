66177462a581c13e59cd40494fb8f099f9e1c80e
Megurin <tutimur_0521@yahoo.co.jp>
2021-09-02 19:23:40 +0900

    write system tests

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -10,6 +10,8 @@ class ReportTest < ActiveSupport::TestCase
   end
 
   test "#created_on" do
-
+    report = Report.new(title: "今日の課題", content: "テスト書く")
+    report.created_at = "Sun, 22 Aug 2021 09:56:51.976710000 JST +09:00"
+    assert_equal "2021-08-22", "#{report.created_on}"
   end
 end
--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -4,44 +4,58 @@ require 'application_system_test_case'
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    @book = books(:one)
+    @book = books(:harry_potter)
+
+    visit root_url
+    fill_in 'Eメール', with: 'alice@example.com'
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログイン'
   end
 
   test 'visiting the index' do
     visit books_url
-    assert_selector 'h1', text: 'Books'
+    assert_selector 'h1', text: '本'
+    assert_text 'ハリーポッターと賢者の石'
+    assert_text '魔法の世界'
+    assert_text 'J・K・ローリング'
   end
 
   test 'creating a Book' do
     visit books_url
-    click_on 'New Book'
+    click_on '新規作成'
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
+    fill_in 'タイトル', with: 'ハリーポッターとアズカバンの囚人'
+    fill_in 'メモ', with: '泣ける'
+    fill_in '著者', with: 'J・K・ローリング'
+    click_on '登録する'
 
-    assert_text 'Book was successfully created'
-    click_on 'Back'
+    assert_text '本が作成されました。'
+    assert_text 'ハリーポッターとアズカバンの囚人'
+    assert_text '泣ける'
+    assert_text 'J・K・ローリング'
   end
 
   test 'updating a Book' do
     visit books_url
-    click_on 'Edit', match: :first
+    click_on '編集'
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
+    fill_in 'タイトル', with: 'ハリーポッターと秘密の部屋'
+    fill_in 'メモ', with: '感動した'
+    fill_in '著者', with: 'J・K・ローリング'
+    click_on '更新する'
 
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
+    assert_text '本が更新されました。'
+    assert_text 'ハリーポッターと秘密の部屋'
+    assert_text '感動した'
+    assert_text 'J・K・ローリング'
   end
 
   test 'destroying a Book' do
     visit books_url
     page.accept_confirm do
-      click_on 'Destroy', match: :first
+      click_on '削除'
     end
 
-    assert_text 'Book was successfully destroyed'
+    assert_text '本が削除されました。'
   end
 end
